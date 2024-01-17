7e8465c1413e77e3d5c6ecfea5f5ef94bf7f321d
giovannalizz <cru23026@byui.edu>
2024-01-17 09:24:52 -0700

    first commit

---
+++ lovecalculator.py
@@ -0,0 +1,7 @@
+print("WELCOME TO THE LOVE CALCULATOR!\nPlease enter the first and the last name.")
+name1 = input("What is your name?")
+name2 = input("What is your partner name?")
+
+combined_names = name1 + name2
+lower_names = combined_names.lower()
+
